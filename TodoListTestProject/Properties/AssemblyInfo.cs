ff038b8d9d78a1962d66bbbef2d58ef8b285eeb5
issuser <issuser@ISS-CR-31205.stf.nus.edu.sg>
2019-04-10 13:02:28 +0800

    Add project files.

---
+++ TodoListTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TodoListTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TodoListTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91829adb-389d-4d7e-a8e6-bd446fb6cf09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TodoListTestProject/TodoListTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TodoListTestProject
+{
+    [TestClass]
+    public class TodoListTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
